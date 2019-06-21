b92265daa5c0d8a12a27e9b4375036c6f27d75f7
feifei <1320954152@qq.com>
2019-06-21 23:01:07 +0800

    区块的简单完成

--- block.go
+++ block.go
@@ -1,10 +1,15 @@
 package main
 
+import(
+	"fmt"
+)
 type block struct{
 	//版本
 	Version int32
 	//前区块的哈希值
 	PrevBlockHash []byte
+	//当前区块的哈希值
+	Hash []byte
 	//梅克尔根
 	MerKerRoot []byte
 	//时间戳
@@ -16,4 +21,40 @@ type block struct{
 
 	//交易信息
 	Data []byte
+}
+
+func NewBlock(data string.prevBlockHash []byte) *Block{
+	//
+	var block Block
+	block = Block{
+		Version:1,
+		PrevBlockHash:prevBlockHash,
+		//Hash
+		MerKerRoot:[]byte,
+		TimeStamp:time.Now().Unix(),
+		Bits:1,
+		Nonce:1,
+		Data:[]byte(data)}
+	block.SetHash()
+	return &block
+}
+
+func (block* Block)SetHash(){
+
+	tmp := [][]byte{
+		IntToByte(block.Version),
+		block.PrevBlockHash,
+		block.MerKerRoot,
+		IntToByte(block.TimeStamp),
+		IntToByte(block.Bits),
+		IntToByte(block.Nonce),
+		block.Data}
+	byte.join(tmp,[]byte{})
+	hash := sha256.Sum256(data)
+	block.Hash = hash[:]
+}
+
+
+func NewGenesisBlock() {
+	NewBlock("Genesis Block",[]byte{})
 }
